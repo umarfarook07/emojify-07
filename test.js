36db0dcbaafc31899890048cca9e1a7bb4586630
Umar Farook <umarkhaji456@gmail.com>
2024-10-07 23:10:36 +0530

    add new test

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-const { convertEmojis } = require('./index');
+const { convertEmojis, addEmoji } = require('./index');
 
-const testText = "I am feeling :smile: and :heart: today!";
+const testText = 'I am feeling :smile: and :heart: today!';
 console.log(convertEmojis(testText));
 
+addEmoji(':salute:', '🫡');
+
+const customEmojiTest = ':salute: to your work man!!';
+console.log(convertEmojis(customEmojiTest));
